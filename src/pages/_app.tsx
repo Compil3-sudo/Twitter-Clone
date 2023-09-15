f5d9a11d7e4c341323441066f6ccc77d5f2d021f
tincurar <tincurares13@gmail.com>
2023-09-16 00:06:17 +0300

    added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,25 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Twitter Clone</title>
+        <meta
+          name="description"
+          content="This is a Twitter Clone by Compil3 learning from Web Dev Simplified"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/rares_favicon/light/rares_favicon-light-16x16.png"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/rares_favicon/dark/rares_favicon-dark-16x16.png"
+          media="(prefers-color-scheme: light)"
+        />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
