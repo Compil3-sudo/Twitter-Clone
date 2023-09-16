bf91e3b513f9d20d96d577d3402963a8d5283807
tincurar <tincurares13@gmail.com>
2023-09-16 14:33:24 +0300

    added NewTweetForm, trpc route to create tweet

---
+++ src/components/InfiniteTweetList.tsx
@@ -0,0 +1,9 @@
+type InfiniteTweetListProps = {
+  tweets: Array<string>;
+};
+
+const InfiniteTweetList = ({ tweets }: InfiniteTweetListProps) => {
+  return <div>InfiniteTweetList</div>;
+};
+
+export default InfiniteTweetList;
