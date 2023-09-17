50ab82806ce7fd001d5524dfcf1d96a2dfdaebd7
tincurar <tincurares13@gmail.com>
2023-09-17 14:40:02 +0300

    added recents and following tabs, moved recents

---
+++ src/components/RecentTweets.tsx
@@ -0,0 +1,22 @@
+import InfiniteTweetList from "~/components/InfiniteTweetList";
+
+import { api } from "~/utils/api";
+
+function RecentTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    {},
+    { getNextPageParam: (lastPage) => lastPage.nextCursor },
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
+}
+
+export default RecentTweets;
